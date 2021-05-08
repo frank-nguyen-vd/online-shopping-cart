56ba782a010ea17ee20289f73dc20751671602a4
Frank Nguyen <frank.nguyen.vd@gmail.com>
2021-05-08 23:14:13 +0800

    🎨 update controller based on new repository

--- src/resources/users/controller.js
+++ src/resources/users/controller.js
@@ -1,49 +1,64 @@
-const Users = require('./model');
+const userRepository = require('./repository');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
-module.exports = {
-    create: (req, res, next) => {
-        Users.create(
-            {
-                username: req.body.username,
-                password: req.body.password
-            },
-            (err, result) => {
-                if (err) next(err);
-                else {
-                    res.status(200).json({
-                        success: true,
-                        message: 'New user created'
-                    });
-                }
-            }
-        );
-    },
-    authenticate: (req, res, next) => {
-        Users.findOne({ username: req.body.username }, (err, userInfo) => {
-            if (err) {
-                next(err);
-            } else {
-                if (bcrypt.compareSync(req.body.password, userInfo.password)) {
-                    const token = jwt.sign(
-                        { id: userInfo.id },
-                        process.env.SECRET_KEY,
-                        { expiresIn: '1h' }
-                    );
+exports.create = async (req, res) => {
+    const payload = {
+        username: req.body.username,
+        password: req.body.password
+    };
 
-                    res.status(200).json({
-                        success: true,
-                        message: 'Log in successfuly',
-                        data: { token: token }
-                    });
-                } else {
-                    res.status(400).json({
-                        success: false,
-                        message: 'Invalid username and password'
-                    });
-                }
-            }
+    try {
+        const user = await userRepository.create(payload);
+        res.status(200).json({
+            success: true,
+            message: 'New user created',
+            data: user
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            success: false,
+            message: 'Cannot create new user'
+        });
+    }
+};
+exports.authenticate = async (req, res) => {
+    const payload = { username: req.body.username };
+    try {
+        const user = await userRepository.findOne(payload);
+        if (bcrypt.compareSync(req.body.password, user.password)) {
+            const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY, {
+                expiresIn: '1h'
+            });
+
+            res.status(200).json({
+                success: true,
+                message: 'Log in successfuly',
+                data: { token: token }
+            });
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(400).json({
+            success: false,
+            message: 'Invalid username and password'
+        });
+    }
+};
+
+exports.findAll = async (req, res) => {
+    try {
+        const users = await userRepository.findAll();
+        res.status(200).json({
+            success: true,
+            data: users
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            success: false,
+            message: err
         });
     }
 };
