b5e2edee1782bf9b0a9e702938b5638a9a1aeb01
Frank Nguyen <frank.nguyen.vd@gmail.com>
2021-05-08 22:01:48 +0800

    Revert "Merge branch 'set_up_auth0' into develop"
    This reverts commit 41e84d62d5306ea7c5ee511a0b873649d51ae1fc, reversing
    changes made to e7826a65e501ac55212e11a2798f88ac7eed6450.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
 
 const express = require('express');
 const app = express();
-const { requiresAuth } = require('express-openid-connect');
 
 // ! This replaces deprecated body-parser to parses JSON bodies
 app.use(express.json());
@@ -12,20 +11,14 @@ require('./config/mongoose.js')(app);
 require('dotenv').config();
 const port = process.env.PORT || 3000;
 
-const auth0 = require('./config/auth0');
-app.use(auth0);
-
 app.get('/', (req, res) => {
-    if (req.oidc.isAuthenticated()) {
-        res.json({ success: true, message: 'Logged In' });
-    } else {
-        res.json({ success: true, message: 'Logged Out' });
-    }
+    res.json({
+        success: true,
+        message: 'Welcome to Shopping Cart API'
+    });
 });
 
-app.get('/profile', requiresAuth(), (req, res) => {
-    res.send(JSON.stringify(req.oidc.user));
-});
+
 
 require('./resources/router')(app);
 
