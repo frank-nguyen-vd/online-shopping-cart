d7544260d84b91f6d98d3b32862539fb0495b6dc
Frank Nguyen <frank.nguyen.vd@gmail.com>
2021-05-10 10:53:25 +0800

    ✨ add carts router to routing table

--- src/resources/router.js
+++ src/resources/router.js
@@ -1,9 +1,14 @@
 const productsRoutes = require('./products/routes');
 const usersRoutes = require('./users/routes');
+const cartsRoutes = require('./carts/routes');
 
 module.exports = (app) => {
     app.use('/products', productsRoutes);
 };
 module.exports = (app) => {
     app.use('/users', usersRoutes);
+};
+
+module.exports = (app) => {
+    app.use('/carts', cartsRoutes);
 };
