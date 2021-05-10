b02807aeccbfa6b5bfe8cc6cef396700025784d5
Frank Nguyen <frank.nguyen.vd@gmail.com>
2021-05-10 11:48:39 +0800

    🐛 place routes handlers in one module.exports
    We should not have multiple module.exports in one file

--- src/resources/router.js
+++ src/resources/router.js
@@ -3,12 +3,7 @@ const usersRoutes = require('./users/routes');
 const cartsRoutes = require('./carts/routes');
 
 module.exports = (app) => {
-    app.use('/products', productsRoutes);
-};
-module.exports = (app) => {
     app.use('/users', usersRoutes);
-};
-
-module.exports = (app) => {
+    app.use('/products', productsRoutes);
     app.use('/carts', cartsRoutes);
 };
