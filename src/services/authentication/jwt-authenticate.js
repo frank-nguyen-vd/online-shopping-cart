674acc18ea02eba9d73db36b9112484fc06b03d7
Frank Nguyen <frank.nguyen.vd@gmail.com>
2021-05-10 23:18:51 +0800

    ♻️ refactor jwt-authenticate service

--- src/services/authentication/jwt-authenticate.js
+++ src/services/authentication/jwt-authenticate.js
@@ -2,23 +2,26 @@ const jwt = require('jsonwebtoken');
 
 require('dotenv').config();
 
+exports.decode = async (jwt_token) => {
+    return await jwt.verify(
+        jwt_token,
+        process.env.SECRET_KEY,
+        (err, decoded) => {
+            if (err) {
+                throw err;
+            }
+            return decoded;
+        }
+    );
+};
 exports.authenticate = async (req, res, next) => {
     try {
         const bearer_token = req.headers['authorization'];
         const jwt_token = bearer_token.split(' ')[1];
-
-        await jwt.verify(jwt_token, process.env.SECRET_KEY, (err, decoded) => {
-            if (err) {
-                console.log(err);
-                res.status(401).json({
-                    success: false,
-                    message: 'Unauthorized'
-                });
-            } else {
-                req.body.userId = decoded.id;
-                next();
-            }
-        });
+        const decode = await this.decode(jwt_token);
+        req.body.userId = decode.userId;
+        req.body.role = decode.role;
+        next();
     } catch (err) {
         console.log(err);
         res.status(500).json({
