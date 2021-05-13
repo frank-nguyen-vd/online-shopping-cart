0c7d8f34c061fc580747af2c59677a811058ccd5
Frank Nguyen <frank.nguyen.vd@gmail.com>
2021-05-13 14:46:08 +0800

    🐛 update filepath to jwt-service

--- src/resources/transactions/routes.js
+++ src/resources/transactions/routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const transactionsController = require('./controller');
-const jwtService = require('../../services/jwt-authenticate');
+const jwtService = require('../../services/authentication/jwt-authenticate');
 
 router.post('/', jwtService.authenticate, transactionsController.checkout);
 
